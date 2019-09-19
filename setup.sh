ade97324a19571840fd4399d0700a0d80bcda006
chuypit <47224899+chuypit@users.noreply.github.com>
2019-09-19 12:27:38 -0500

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -5,12 +5,10 @@ PARENT=`dirname $CURRENT`
 SCRIPTS='/home/pi/scripts'
 #start install
 echo "**************************************"
-echo "Installing Nespi532"
+echo "Installing Snes pi nfc"
 echo "**************************************"
 echo -n "**************************************"
-echo -n "Warning!!! Installing the Pi Control Board on the incorrect pins on the Pi can damage your Pi!"
-echo "Please use Pi Control Hardware and Software at your own risk. We do not take responsibility for any damages to your raspberry pi that may occure."
-echo "By downloading and installing our hardware and software you are agreeing to these terms."
+echo -n "demo instalacion snes pi!"
 echo "**************************************"
 echo -n "Would you like to continue with the installation? (y/n): "
 read REPLY
@@ -18,26 +16,20 @@ if [ $REPLY = "y" ] || [ $REPLY = "Y" ]
 then
     echo "**************************************"
     echo "Installing NFC "
-    apt-get install -y python-dev python-pip git
-    git clone https://github.com/adafruit/Adafruit_Python_PN532.git
-    cd Adafruit_Python_PN532
+    apt-get install python2.7-dev
+    git clone https://github.com/lthiery/SPI-Py.git
+    cd SPI-Py
     python setup.py install
     cd ../
-    rm -R Adafruit_Python_PN532
-    echo "Enabling Serial Interface............."
-    #echo 'enable_uart=1' >> /boot/config.txt
-    sed -i '\:enable_uart=0:d' /boot/config.txt 
-    sed -i '\:enable_uart=1:d' /boot/config.txt
-    echo 'enable_uart=1' >> /boot/config.txt
     #update startup
     echo "Updating Startup Commands............."
     sed -i '\:emulationstation #auto:d' /opt/retropie/configs/all/autostart.sh
     sed -i '\:emulationstation:d' /opt/retropie/configs/all/autostart.sh
-    sed -i '\:python /home/pi/Nespirc532/v4.py 2>&1:d' /opt/retropie/configs/all/autostart.sh
-    echo 'python /home/pi/Nespirc532/v4.py 2>&1' >> /opt/retropie/configs/all/autostart.sh
+    sed -i '\:python /home/pi/Snespi/romv4.py 2>&1:d' /opt/retropie/configs/all/autostart.sh
+    echo 'python /home/pi/Snespi/romv4.py 2>&1' >> /opt/retropie/configs/all/autostart.sh
     echo 'emulationstation' >> /opt/retropie/configs/all/autostart.sh
     rm -R /home/pi/RetroPie/retropiemenu/rom.sh
-    echo 'python /home/pi/Nespirc532/game.py 2>&1&' > /home/pi/RetroPie/retropiemenu/rom.sh
+    echo 'python /home/pi/Snespi/consola.py 2>&1&' > /home/pi/RetroPie/retropiemenu/rom.sh
     chmod -R 7777 /home/pi/RetroPie/retropiemenu/rom.sh
     echo "Installation Complete................."
     echo -n "You must reboot for changes to take effect, reboot now? (y/n): "
@@ -51,11 +43,11 @@ fi
 cd /etc/
 RC=rc.local
 
-if grep -q "sudo python \/home\/pi\/Nespirc532\/button.py \&" "$RC";
+if grep -q "sudo python \/home\/pi\/Snespi\/Reset.py \&" "$RC";
 	then
 		echo "File /etc/rc.local already configured. Doing nothing."
 	else
-		sed -i -e "s/^exit 0/sudo python python \/home\/pi\/Nespirc532\/button.py \&\n&/g" "$RC"
+		sed -i -e "s/^exit 0/sudo python python \/home\/pi\/Snespi\/Reset.py \&\n&/g" "$RC"
 		echo "File /etc/rc.local configured."
 fi
 
